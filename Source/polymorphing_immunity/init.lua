5931caeca0084ad01e899850f4b0e190f4be0182
Gabor Soos <soosgjr@users.noreply.github.com>
2021-12-06 22:13:21 -0500

    Fixing the compatibility issues with the latest version.

--- Source/polymorphing_immunity/init.lua
+++ Source/polymorphing_immunity/init.lua
@@ -1,2 +1,8 @@
+local content = ModTextFileGetContent("data/translations/common.csv")
+ModTextFileSetContent("data/translations/common.csv", content .. [[
+perk_polymorphing_immunity,Polymorphing Immunity,,,,,,,,,,,,,
+perkdesc_polymorphing_immunity,Polymorphing has no effect on you.,,,,,,,,,,,,,
+]])
+
 dofile("data/scripts/perks/perk_list.lua")
 ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/polymorphing_immunity/files/script.lua")
